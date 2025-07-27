82e365a277df3ac81665550f2c871d7bf7103da1
Dilara Tank <66127366+dilaratank@users.noreply.github.com>
2025-07-27 18:54:12 +0200

    Update unetmodels.py

--- unetmodels.py
+++ unetmodels.py
@@ -1,9 +1,9 @@
 # unet model weights
 
 u_net_model = 'dilaratank/MScUtiSeg/model-nw81559f:vbest' #Modality: VIDEO SCREENSHOTS
-u_net_model = '' #Modality: ALL
-u_net_model = '' #Modality: 3D screenshots
-u_net_model = '' #Modality: STILL
+u_net_model = 'dilaratank/MScUtiSeg/model-or6fn3ep:vbest' #Modality: ALL
+u_net_model = 'dilaratank/MScUtiSeg/model-b7dchl80:vbest' #Modality: 3D screenshots
+u_net_model = 'dilaratank/MScUtiSeg/model-9y1afl1k:vbest' #Modality: STILL
 
 import wandb
 run = wandb.init()
